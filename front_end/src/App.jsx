a84452debf14fa88adde702cc2d6189e7b1e5c5e
YosephDagne <152218628+YosephDagne@users.noreply.github.com>
2025-02-28 17:03:13 +0300

    use tailwindcss

--- front_end/src/App.jsx
+++ front_end/src/App.jsx
@@ -1,12 +1,11 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 function App() {
-  
   return (
     <>
       <h1>Amazone Clone</h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
